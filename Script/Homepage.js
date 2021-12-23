28e31bf76d69807362440a0bebc8bbf1d87d1823
Thanh Nguyen <nqctttt@gmai.com>
2021-12-23 21:09:58 +0700

    last function

--- Script/Homepage.js
+++ Script/Homepage.js
@@ -14,3 +14,20 @@ news.controller("NewsController",($scope,$http)=>{
     );
     
 })
+var urlParams=new URLSearchParams(location.search);
+var id=urlParams.get("userID");
+function patientPortal(){
+    location.href="Patient_Portal.html?userID="+id
+}
+function medicalRecord(){
+    location.href="Patient_Record.html?userID="+id
+}
+function virtualSchedule(){
+    location.href="Schedule_Virtual.html?userID="+id
+}
+function homecareSchedule(){
+    location.href="Schedule_Homecare.html?userID="+id
+}
+function basicSchedule(){
+    location.href="Schedule_Basic.html?userID="+id
+}
