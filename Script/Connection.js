fb48392bd48b1fa063372162be628d424d87c745
Thái Vĩnh Khang <92316262+KhangKT1@users.noreply.github.com>
2021-12-24 00:04:52 +0700

    Create Connection.js

---
+++ Script/Connection.js
@@ -0,0 +1,47 @@
+var urlParams=new URLSearchParams(location.search);
+var id=urlParams.get("userID");
+function toAboutUs(){
+    if (id==null)
+    { window.location.href="About_Us.html"}
+    else 
+    {
+        window.location.href="About_Us.html?userID="+id
+    }
+}
+function patientPortal(){
+    
+        location.href="Patient_Portal.html?userID="+id
+}
+function medicalRecord(){
+    if (id==null)
+    { window.location.href="Terms_Of_Service.html"}
+    else 
+    location.href="Patient_Record.html?userID="+id
+}
+function virtualSchedule(){
+     
+    if (id==null)
+    { window.location.href="Terms_Of_Service.html"}
+    else 
+    {
+       window.location.href="Schedule_Virtual.html?userID="+id
+    }
+}
+function homecareSchedule(){
+    if (id==null)
+    {location.href="Terms_Of_Service.html"}
+    else 
+    {
+        location.href="Schedule_Homecare.html?userID="+id
+    }
+    
+}
+function basicSchedule(){
+    if (id==null)
+    {location.href="Terms_Of_Service.html"}
+    else 
+    {
+        location.href="Schedule_Basic.html?userID="+id
+    }
+    
+}
